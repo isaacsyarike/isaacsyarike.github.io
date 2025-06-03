e7299188c9b68941e58b6e51a7a2a9004814de95
isaacsyarike <isaacbsisyarike@gmail.com>
2025-06-03 14:49:02 +0200

    Ajout des fichiers initiaux du site KIVUNETWORK
    Version 1 du site futuriste avec design spatial.

---
+++ kivunetwork_script.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const aiMessage = "Bienvenue dans la station ASTRA. Chargement des protocoles galactiques...";
+  alert(aiMessage);
+});
